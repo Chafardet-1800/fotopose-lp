9baca598d0ac36bf1ebf16fcad70ee6104d11bce
desarrollo-CityPos <desarrollo@citypos.com.ve>
2026-01-27 16:28:30 -0400

    Solucion al fallo esta vez si es real

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,29 +1,29 @@
-import bcrypt from "bcryptjs";
-import { prisma } from "./prisma";
+// import bcrypt from "bcryptjs";
+// import { prisma } from "./prisma";
 
 /**
  * Recibe username y contraseña plana.
  * Devuelve true si son correctos, false si no.
  */
-export async function validateCredentials(
-  username: string,
-  pass: string,
-): Promise<boolean> {
-  try {
-    // 1. Buscar usuario en Prisma
-    const user = await prisma.user.findFirst({
-      where: { username },
-    });
+// export async function validateCredentials(
+//   username: string,
+//   pass: string,
+// ): Promise<boolean> {
+//   try {
+//     // 1. Buscar usuario en Prisma
+//     const user = await prisma.user.findFirst({
+//       where: { username },
+//     });
 
-    // 2. Si no existe, fallar
-    if (!user) return false;
+//     // 2. Si no existe, fallar
+//     if (!user) return false;
 
-    // 3. Comparar la contraseña enviada con el hash en la BD
-    const isValid = await bcrypt.compare(pass, user.password);
+//     // 3. Comparar la contraseña enviada con el hash en la BD
+//     const isValid = await bcrypt.compare(pass, user.password);
 
-    return isValid;
-  } catch (error) {
-    console.error("Error validando usuario:", error);
-    return false;
-  }
-}
+//     return isValid;
+//   } catch (error) {
+//     console.error("Error validando usuario:", error);
+//     return false;
+//   }
+// }
